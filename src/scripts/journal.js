7b284495699ede328cc65339863684559781d33d
Samuel Birky <sbirky21@gmail.com>
2019-06-10 19:37:02 -0500

    journal 4 entry

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,26 +1,26 @@
-const objectsJournalEntry = [
-    {
-    Date_of_Entry: "06/06/2019",
-    Concepts_Covered: "Manipulating DOM & JS Objects",
-    Journal_Entry: "Today was a bit frustrating trying to understand how the concepts we are learning apply to the real world. However, I am confident that it will become more clear as we add more context to these components.",
-    Mood: "Weird"
-},
+// const objectsJournalEntry = [
+//     {
+//     Date_of_Entry: "06/06/2019",
+//     Concepts_Covered: "Manipulating DOM & JS Objects",
+//     Journal_Entry: "Today was a bit frustrating trying to understand how the concepts we are learning apply to the real world. However, I am confident that it will become more clear as we add more context to these components.",
+//     Mood: "Weird"
+// },
 
-{
-    Date_of_Entry: "06/06/2019",
-    Concepts_Covered: "Functions & Logic",
-    Journal_Entry: "What is this madness?",
-    Mood: "Frustrated"
-},
+// {
+//     Date_of_Entry: "06/06/2019",
+//     Concepts_Covered: "Functions & Logic",
+//     Journal_Entry: "What is this madness?",
+//     Mood: "Frustrated"
+// },
 
-{
-    Date_of_Entry: "06/06/2019",
-    Concepts_Covered: "API, Fetching & Events",
-    Journal_Entry: "Today I am more confident I know how to break down topics to understand them. It may take time, but that's okay. I understand what Joe means when he says 'take it line by line.",
-    Mood: "Happy"
-}
+// {
+//     Date_of_Entry: "06/06/2019",
+//     Concepts_Covered: "API, Fetching & Events",
+//     Journal_Entry: "Today I am more confident I know how to break down topics to understand them. It may take time, but that's okay. I understand what Joe means when he says 'take it line by line.",
+//     Mood: "Happy"
+// }
 
-]
+// ]
 
 // From journal entry 2, oops got ahead of ourselves. We were supposed to make each object have it's very own variable. And then add each of those variables into the array. I defined one variable and within that array, I added each object which made things a bit redundant.
 // const listOfJournalEntries = []
@@ -54,4 +54,12 @@ const renderJournalEntries = (entries) => { //function to add items to eventuall
 
 }
 
-renderJournalEntries(objectsJournalEntry)
+// renderJournalEntries(objectsJournalEntry)
+
+// Your learning objective in this chapter is to set up JSON Server to store the data for your daily journal entries, query that data from the API, and then use your knowledge of Promises (remember, fetch is a fancy Promise) and then() to render the entries to the DOM.
+
+fetch("http://localhost:3000/entries") // Fetch from the API
+    .then(journalData => journalData.json())  // Parse as JSON
+    .then(entries => {console.log(entries)
+        // What should happen when we finally have the array?
+    })
