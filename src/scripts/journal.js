2a9dc5ba0863ab0546b90e73e9ea22e85cfd2397
Samuel Birky <sbirky21@gmail.com>
2019-06-15 13:12:32 -0500

    first part of daily-journal-5

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,3 +1,12 @@
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+
+    Change the fake variable names below to what they should be
+    to get the data and display it.
+*/
+objectWithGetterMethod.methodToGetData().then(functionThatRendersData)
+
 // const objectsJournalEntry = [
 //     {
 //     Date_of_Entry: "06/06/2019",
@@ -29,37 +38,11 @@
 // listOfJournalEntries.push(objectsJournalEntry[2])
 // console.log(listOfJournalEntries);
 
-//You have built your form for entering in journal entries, so now you need to define how the data that you will be collecting in the form should be stored. Your learning objective for this chapter is to build a function that returns an HTML representation of a journal entry data structure, and render it to the DOM.
 
-function makeJournalEntryComponents(journalEntry) {
-    // Create your own HTML structure for a journal entry
-    return `
-    <div>
-    <h1>${journalEntry.Date_of_Entry}</h1>
-    <h2>${journalEntry.Concepts_Covered}</h2>
-    <h3>${journalEntry.Mood}</h3>
-    <p>${journalEntry.Journal_Entry}</p>
-    </div>
-    `
-}
 
-const renderJournalEntries = (entries) => { //function to add items to eventually add to DOM
-    let divProcess = "";
-    for (let i=0; i < entries.length; i++){
-        divProcess += makeJournalEntryComponents(entries[i]); //Each value produced by the function added to divProcess
-    }
- 
-    let entryHoldingLocation = document.getElementById("entryLog") //where do i add it? entryLog
-    entryHoldingLocation.innerHTML = divProcess; // okay, add it
 
-}
 
 // renderJournalEntries(objectsJournalEntry)
 
 // Your learning objective in this chapter is to set up JSON Server to store the data for your daily journal entries, query that data from the API, and then use your knowledge of Promises (remember, fetch is a fancy Promise) and then() to render the entries to the DOM.
 
-fetch("http://localhost:3000/entries") // Fetch from the API
-    .then(journalData => journalData.json())  // Parse as JSON
-    .then(entries => {console.log(entries)
-        // What should happen when we finally have the array?
-    })
